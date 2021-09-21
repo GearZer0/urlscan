ea113e38d83332a1286980ee6fc1ee3f5f279b4b
GearZer0 <61912722+GearZer0@users.noreply.github.com>
2021-09-21 21:28:39 +0800

    added query -q and help --help

--- urlscan.py
+++ urlscan.py
@@ -1,13 +1,18 @@
 import requests
+import argparse
 
 
 urlscan_apikey = ""  # put urlscan api key here
 
-brand = input("Enter brand: ")
-duration = input("Enter duration: ")
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter, prog="urlscan [-b] and [-d] or [-u]")
+parser.add_argument("-b", type=str, default="", help="put brand")
+parser.add_argument("-d", type=str, default="", help="put duration")
+parser.add_argument("-q", type=str, default="", help="put custom query")
+args = parser.parse_args()
 
 
-def performSearch():
+def performSearch(brand, duration):
     api_link = "https://urlscan.io/api/v1/search/"
     params = {
         "q": "brand.key:{} AND date:>now-{}".format(brand, duration)
@@ -21,7 +26,34 @@ def performSearch():
         resp = requests.get(api_link, headers=headers, params=params).json()
     except:
         print("Failed to open {}".format(api_link))
+        return
     print(resp)
 
+
+def performQuery(query):
+    api_link = "https://urlscan.io/api/v1/search/"
+    params = {
+        "q": query
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "API-Key": urlscan_apikey,
+        'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Safari/537.36'
+    }
+    try:
+        resp = requests.get(api_link, headers=headers, params=params).json()
+    except:
+        print("Failed to open {}".format(api_link))
+        return
+    print(resp)
+
+
 if __name__ == "__main__":
-    performSearch()
+    if args.b:
+        brand = args.b
+        duration = args.d
+        performSearch(brand, duration)
+    elif args.q:
+        performQuery(args.q)
+    else:
+        print(parser.print_help())
